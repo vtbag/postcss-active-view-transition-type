422e88c57077abd398356fb0cd6757182913d74b
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-07-13 14:54:59 +0200

    eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly'
+      }
+    },
+    plugins: {},
+    rules: {
+      // add custom rules here
+    }
+  }
+]
